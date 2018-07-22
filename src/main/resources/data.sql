b367d30e646eb9cffd716663e511f9d98e8fe8c1
nitesh009 <niteshgnit3@gmail.com>
2018-07-22 15:16:32 +0530

    Fixing build issues

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 DELETE FROM links;
 
 insert into links(id, title, url, created_on) values
-(1, 'My portfolio?','www.knitesh.com', '2016-10-07'),
-(2, 'My portfolio?','www.knitesh.com', '2016-10-07'),
-(3, 'My portfolio?','www.knitesh.com', '2016-10-07')
+(1, 'My portfolio','http://www.knitesh.com', '2015-12-01'),
+(2, 'My book','https://www.amazon.in/Jump-Start-your-software-career-ebook/dp/B079DXGXR8/ref=cm_cr_arp_d_product_top?ie=UTF8', '2018-01-01'),
+(3, 'My Github','https://www.github.com/nitesh009', '2015-01-08')
 ;
